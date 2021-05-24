cb9c30c717eeb694226803ff8fb6741be6ec85fe
quarkusio <no-reply@quarkus.io>
2021-05-24 13:29:14 +0000

    Initial commit

---
+++ src/test/java/org/tinyg/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package org.tinyg;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
